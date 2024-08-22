3f43860aab31adb19964c59ed5607608192060aa
django <django@5766366096c9>
2024-08-22 13:48:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-bird-30",
+  url: "https://twilight-bird-30.botics.co",
+  api: "https://twilight-bird-30.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
